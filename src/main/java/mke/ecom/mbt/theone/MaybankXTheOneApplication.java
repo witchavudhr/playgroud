8a1cec8b78c8fb5abb7ac4a60b929a191d93d5f5
witchavudh <witchavudh.r@maybank-ke.co.th>
2021-05-23 01:20:49 +0700

    test

---
+++ src/main/java/mke/ecom/mbt/theone/MaybankXTheOneApplication.java
@@ -0,0 +1,26 @@
+package mke.ecom.mbt.theone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@SpringBootApplication
+public class MaybankXTheOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaybankXTheOneApplication.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig
+                = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
+    }
+}
---
+++ src/test/java/mke/ecom/mbt/theone/MaybankXTheOneApplicationTests.java
@@ -0,0 +1,13 @@
+package mke.ecom.mbt.theone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaybankXTheOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
